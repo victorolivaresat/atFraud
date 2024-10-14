87655c9e8fd72f10736b6fe92378ab5cd6f08c22
Victor Olivares <viktor_olivares@outlook.com>
2024-10-14 15:33:09 -0500

    add: backend .net

--- frontend_react/tailwind.config.js
+++ frontend_react/tailwind.config.js
@@ -5,7 +5,15 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          100: "#3A00B0",
+          300: "#29007D",
+          900: "#120037",
+        },
+      },
+    },
   },
   plugins: [],
 }
