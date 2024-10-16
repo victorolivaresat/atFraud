95c529175a3b71aef7a2cb7ab0d949a97db487b7
angelodiaz-bi <angelo.diaz@apuestatotal.com>
2024-10-16 11:02:50 -0500

    feat: frontend 20241610

--- frontend_react/tailwind.config.js
+++ frontend_react/tailwind.config.js
@@ -1,8 +1,10 @@
 /** @type {import('tailwindcss').Config} */
+import flowbitePlugin from 'flowbite/plugin';
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
   ],
   theme: {
     extend: {
@@ -15,5 +17,7 @@ export default {
       },
     },
   },
-  plugins: [],
+  plugins: [    
+    flowbitePlugin
+  ],
 }
